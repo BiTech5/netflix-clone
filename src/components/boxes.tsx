1e9cd39894f8bde2261acf15c103728429e40adc
BiTech5 <bikramkhatri050@gmail.com>
2025-04-22 16:47:51 +0545

    box update using daisy ui

--- src/components/boxes.tsx
+++ src/components/boxes.tsx
@@ -8,29 +8,56 @@ export const Boxes = () => {
                         title: "Enjoy on your TV",
                         description:
                             "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+                        icon: (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path fillRule="evenodd" d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z" clipRule="evenodd" />
+                            </svg>
+                        )
                     },
                     {
                         title: "Download your shows",
                         description:
                             "Save your favorites easily and always have something to watch.",
+                        icon: (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path fillRule="evenodd" d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V10.5Z" clipRule="evenodd" />
+                            </svg>
+
+                        )
                     },
                     {
                         title: "Watch everywhere",
                         description:
                             "Stream on your phone, tablet, laptop, and TV without paying more.",
+                        icon: (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path d="M10.5 18.75a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" />
+                                <path fillRule="evenodd" d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z" clipRule="evenodd" />
+                            </svg>
+
+                        )
                     },
                     {
                         title: "Create profiles for kids",
                         description:
                             "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
+                        icon: (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
+                            </svg>
+
+                        )
                     },
                 ].map((box, index) => (
-                    <div
-                        key={index}
-                        className="bg-black/50 w-full p-6 rounded-lg shadow-lg hover:bg-black/60 transition duration-300"
-                    >
-                        <h1 className="text-xl md:text-2xl font-bold mb-2">{box.title}</h1>
-                        <p className="text-sm md:text-base text-white/70">{box.description}</p>
+                    <div key={index} className="card  card-xs shadow-sm  bg-black/50 w-full p-6  hover:bg-/black/60 transition duration-300">
+                        <div className="card-body" >
+                            <h2 className="card-title text-xl md:text-2xl">{box.title}</h2>
+                            <p className="text-sm">{box.description}</p>
+                            <div className="justify-end card-actions">
+                                {box.icon}
+
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
